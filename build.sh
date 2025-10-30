c8e881f9d31908f0d3ab9b765502110301c79aa6
Hannah Law <hannah.law@portswigger.net>
2025-10-30 13:55:39 +0000

    Add wrapper

--- build.sh
+++
@@ -1,8 +0,0 @@
-
-#!/bin/sh
-set -ue
-
-wget https://github.com/federicodotta/RSyntaxTextArea/releases/download/2.6.1.edited/rsyntaxtextarea-2.6.1.edited.jar
-mvn install:install-file -Dpackaging=jar -Dfile=rsyntaxtextarea-2.6.1.edited.jar -DgroupId=com.fifesoft -DartifactId=rsyntaxtextarea -Dversion=2.6.1.edited
-#gradle fatJar
-mvn package
